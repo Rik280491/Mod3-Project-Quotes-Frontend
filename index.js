fd9fc577f7de9d1485621eb8f191605629440d1d
Rik280491 <57149887+Rik280491@users.noreply.github.com>
2020-03-24 16:58:32 +0000

    getAPIs and initial rendering of ALL in array, need to figure out single rendering on click events (#1)
    Co-authored-by: simon <sjtucker28@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// commit 
+const QUOTES_API = "http://localhost:3000/quotes";
+const AUTHORS_API = "http://localhost:3000/authors";
+
+const API = {
+  getQuotes: () => fetch(QUOTES_API).then(resp => resp.json()),
+
+  getAuthors: () => fetch(AUTHORS_API).then(response => response.json())
+};
+
+const body = document.querySelector("body");
+const landingPage = document.querySelector("#landing-page");
+
+const renderLanding = () => {
+  const title = document.createElement("h1");
+  title.innerText = "Who said it??";
+
+  const gameBtn = document.createElement("button");
+  gameBtn.innerText = "Start";
+
+  gameBtn.addEventListener("click", () => {
+    renderGame();
+  });
+
+  landingPage.append(title, gameBtn);
+};
+
+const singQuote = quotes => {
+  debugger;
+  quotes.forEach(quote => renderQuote(quote));
+};
+
+const singAuthor = authors => {
+  authors.forEach(author => renderAuthor(author));
+};
+
+const renderAuthor = author => {
+  const image = document.createElement("img");
+  image.src = author.img_url;
+
+  body.append(image);
+};
+
+const renderQuote = quote => {
+  const quoteCard = document.createElement("div");
+  quoteCard.className = "card";
+  const quoteContent = document.createElement("p");
+  quoteContent.innerText = quote.content;
+
+  quoteCard.append(quoteContent);
+  body.append(quoteCard);
+};
+
+const renderGame = () => {
+  landingPage.innerHTML = "";
+  API.getQuotes().then(quotes => singQuote(quotes));
+  API.getAuthors().then(authors => singAuthor(authors));
+};
+renderLanding();
